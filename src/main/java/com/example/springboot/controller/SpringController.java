9b928d7ba82a100b052309453e35a18cc8851b56
purna16 <shivar640@gmail.com>
2023-05-04 13:04:24 +0530

    changing the code

--- src/main/java/com/example/springboot/controller/SpringController.java
+++ src/main/java/com/example/springboot/controller/SpringController.java
@@ -8,22 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringController {
 
-	private transient Integer val = 0;
-	
-	@GetMapping(value = "/")
-	public String getValue() {
-
-//		LocalDateTime date = LocalDateTime.now();
-//
-//		Integer year = date.getYear();
-//		Integer month = date.getMonthValue();
-//		Integer day = date.getDayOfMonth();
-//		Integer hour = date.getHour();
-//		Integer min = date.getMinute();
-//		Integer sec = date.getSecond();
-		val++;
-//		Integer count = year + month + day + hour + min + sec + 1;
-		String result = "Mr. DevOps" + val;
-		return result;
+	@GetMapping("/hello")
+	public String hello(){
+		return("Hello Purna");
 	}
+
 }
